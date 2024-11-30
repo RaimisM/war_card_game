936afefd1cd586e3af8e2d57fe7bd9144ab1e63d
Raimondas Miciuta <raimondasmicc@gmail.com>
2024-11-30 10:40:37 +0200

    Updated class Card and class Deck

--- game_code.py
+++ game_code.py
@@ -1,24 +1,52 @@
 import random
 
 class Card:
-    def __init__(self, ):
+    rank_values = {
+        "2": 2, 
+        "3": 3, 
+        "4": 4, 
+        "5": 5, 
+        "6": 6, 
+        "7": 7, 
+        "8": 8, 
+        "9": 9, 
+        "10": 10, 
+        "Jack": 11, 
+        "Queen": 12, 
+        "King": 13, 
+        "Ace": 14
+    }
+
+    def __init__(self, rank, suit):
+        self.rank = rank
+        self.suit = suit
+        self.rank_values = self.rank_values[rank]
     
     def __str__(self):
+        return f"{self.rank} of {self.suit}"
      
 
 class Deck:
     def __init__(self):
+        self.cards = self.create_deck()
      
     def create_deck(self):
+        suits = ["♦️", "♠️", "♥️", "♣️"]
+        ranks = list(Card.rank_values)
+        return [Card(rank, suit) for suit in suits for rank in ranks]
 
     def shuffle(self):
+        random.shuffle(self.cards)
 
     def deal(self):
+        middle = len(self.cards) // 2
+        return self.cards[:middle], self.cards[middle:]
 
 class Player:
-    def __init__(self, ):
+    def __init__(self, name, hand):
 
     def play(self):
+
     
 class Game:
     def __init__():
